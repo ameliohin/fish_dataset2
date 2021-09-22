48da071efed022a82c5ee9ec866f49aaea9ab271
Александр Мелехин <a.meliohin@gmail.com>
2021-09-22 19:59:28 +0300

    generate pics script : change quality, new dataset

--- images/generate_pictures.sh
+++ images/generate_pictures.sh
@@ -2,14 +2,8 @@
 
 # generate pics and delete empty frames manually
 
-#ffmpeg -i 2k_big_1.mp4 -r 2 2k_big_fish__%04d.png
-#ffmpeg -i 2k_small_1.mp4 -r 2 2k_small_fish__%04d.png
-#ffmpeg -i 640_big_1.mp4 -r 2 640_big_fish__%04d.png
-#ffmpeg -i 640_small_1.mp4 -r 2 640_small_fish__%04d.png
-
-
 for f in *.mp4
 do
 	echo "Processing $f..."
-	ffmpeg -i $f -r 2 $f%04d.jpeg
+	ffmpeg -i $f -r 4 -q:v 2 $f%04d.jpeg
 done
